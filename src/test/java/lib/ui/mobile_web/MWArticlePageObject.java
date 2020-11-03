6ed80c1017dec27be65de0f8855ed154ca13483e
vmorochkovskaya <vasilisademuanova2700@mail.ru>
2020-11-04 00:42:59 +0300

    remove unused locators

--- src/test/java/lib/ui/mobile_web/MWArticlePageObject.java
+++ src/test/java/lib/ui/mobile_web/MWArticlePageObject.java
@@ -9,12 +9,7 @@ public class MWArticlePageObject extends ArticlePageObject {
         ADD_TO_READING_LIST =
                 "xpath://a[@id='ca-watch' and contains(@class, 'menu__item--page-actions-watch') and not(contains(@class, 'watched'))]";
         ADDED_TO_READING_LIST_ICON = "xpath://a[@id='ca-watch' and contains(@class, 'watched')]";
-        GOT_IT = "id:org.wikipedia:id/onboarding_button";
-        OVERLAY_INPUT = "id:org.wikipedia:id/text_input";
-        OK = "xpath://android.widget.Button[@text='OK']";
-        CLOSE = "accessibilityId:Navigate up";
         TITLE = "xpath://div[contains(@class, 'page-heading')]//h1[contains(text(), '%1$s')]";
-        CONTENT = "id:org.wikipedia:id/page_contents_container";
         LIST_BY_SUBSTRING_TEMPLATE = "xpath://android.widget.TextView[@text='%1$s']";
     }
 
